60391599af015d94e92b5ea00ffb18e70757a3e8
onandi-10 <gutierrezmon10@outlook.com>
2022-01-08 17:32:40 -0600

    Se realizo un prefab de las respuestas del juego.

---
+++ Assets/Scripts/Models/QuestionModel.cs
@@ -0,0 +1,20 @@
+[System.Serializable]
+
+public class QuestionModel
+{
+    public string Question;
+
+    public string Answer1;
+
+    public string Answer2;
+
+    public string Answer3;
+    
+    public string Answer4;
+
+    public int CorrectAnswerIndex;
+
+
+
+    
+}
