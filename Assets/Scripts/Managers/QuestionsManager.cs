1bd28207dc3fd4f0aeb856091bb7dfa8ee385612
onandi-10 <gutierrezmon10@outlook.com>
2022-01-17 20:00:25 -0600

    Buscando el error y aun no se encuentra

--- Assets/Scripts/Managers/QuestionsManager.cs
+++ Assets/Scripts/Managers/QuestionsManager.cs
@@ -49,7 +49,7 @@ public class QuestionsManager : Singleton<QuestionsManager>
         OnNewQuestionLoaded?.Invoke();
     }
 
-    public bool AnswerQuestion(int answerIndex)
+    public bool AnswerQuestion (int answerIndex)
     {
 
         OnAnswerProvided?.Invoke();
