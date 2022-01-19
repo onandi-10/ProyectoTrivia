90bd87881637a11bef14174933807e78262d3cc1
AnaMarin28 <94573033+AnaMarin28@users.noreply.github.com>
2022-01-18 22:26:38 -0600

    Prefabs

--- Assets/Script/ControlCenter.cs
+++ Assets/Script/ControlCenter.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlCenter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool restart = Input.GetKeyDown(KeyCode.R);
+
+        if (restart)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
+
+
+
 }
